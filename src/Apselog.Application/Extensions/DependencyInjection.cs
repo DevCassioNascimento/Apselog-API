86d977632d196a204e8e07b7e7ccaf1d32740401
Cassio S Nascimento <cassio.nascimento@adventistas.org>
2026-04-07 14:03:44 -0300

    Feat(Application|Extensions|DependencyInjection): Registrado os casos de uso na DI de application

--- src/Apselog.Application/Extensions/DependencyInjection.cs
+++ src/Apselog.Application/Extensions/DependencyInjection.cs
@@ -1,8 +1,12 @@
 using Microsoft.Extensions.DependencyInjection;
 using Apselog.Application.UseCases.Entrega;
 using Apselog.Application.UseCases;
+using Apselog.Application.UseCases.Motorista;
+using Apselog.Application.UseCases.Veiculo;
 using Apselog.Application.UseCases.Interfaces;
 using Apselog.Application.UseCases.Interfaces.Entrega;
+using Apselog.Application.UseCases.Interfaces.Motorista;
+using Apselog.Application.UseCases.Interfaces.Veiculo;
 
 namespace Apselog.Application.Extensions;
 
@@ -10,13 +14,25 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
+        //User
         services.AddScoped<ICriarUserUseCase, CriarUserUseCase>();
         services.AddScoped<IAtualizarUserUseCase, AtualizarUserUseCase>();
         services.AddScoped<IDeletarUserUseCase, DeletarUserUseCase>();
         services.AddScoped<ICriarEntregaUseCase, CriarEntregaUseCase>();
+        //Entrega
         services.AddScoped<IAtualizarEntregaUseCase, AtualizarEntregaUseCase>();
         services.AddScoped<IListarEntregaUseCase, ListarEntregaUseCase>();
         services.AddScoped<IExcluirEntregaUseCase, ExcluirEntregaUseCase>();
+        // Motorista
+        services.AddScoped<ICriarMotoristaUseCase, CriarMotoristaUseCase>();
+        services.AddScoped<IAtualizarMotoristaUseCase, AtualizarMotoristaUseCase>();
+        services.AddScoped<IListarMotoristaUseCase, ListarMotoristaUseCase>();
+        services.AddScoped<IExcluirMotoristaUseCase, ExcluirMotoristaUseCase>();
+        // Veiculo
+        services.AddScoped<ICriarVeiculoUseCase, CriarVeiculoUseCase>();
+        services.AddScoped<IAtualizarVeiculoUseCase, AtualizarVeiculoUseCase>();
+        services.AddScoped<IListarVeiculoUseCase, ListarVeiculoUseCase>();
+        services.AddScoped<IExcluirVeiculoUseCase, ExcluirVeiculoUseCase>();
 
         return services;
     }
